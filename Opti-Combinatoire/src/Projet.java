960589a6e7594e2dcef0f8926971bf052864b30b
Henry-D-Saenz <33096048+Henry-D-Saenz@users.noreply.github.com>
2018-01-29 10:34:03 +0100

    Add files via upload

---
+++ Opti-Combinatoire/src/Projet.java
@@ -0,0 +1,32 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.Solver;
+import org.chocosolver.solver.variables.IntVar;
+
+public class Projet {
+	
+	private static final int NB_GRUES = 4;
+	private static final int NB_OUVRIERS = 10;
+	private static final int NB_NAVIRES = 4;
+	private static final int NB_TRACE = 50;
+	private static final int TAILLE_QUAI = 30;
+	
+	private int [][] tailleNavires;
+	
+	private IntVar[][] grues;
+	private IntVar[][] ouvriers;
+	private IntVar[][] espace_quai;
+
+	private Model model;	
+	private Solver solver;
+	
+	public Projet() {
+		// TODO Auto-generated constructor stub
+		model = new Model();
+		solver = model.getSolver();
+		tailleNavires= new int [NB_NAVIRES][2];
+		grues=model.intVarMatrix(NB_GRUES, NB_TRACE, -1, NB_NAVIRES);
+		ouvriers=model.intVarMatrix(NB_OUVRIERS, NB_TRACE, -1, NB_OUVRIERS);
+		espace_quai=model.intVarMatrix(TAILLE_QUAI, NB_TRACE, -1,NB_NAVIRES);
+	}
+
+}
